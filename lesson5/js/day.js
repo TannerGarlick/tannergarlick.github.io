d2a7c8e1ee3b950a1bc20c01cbc0dbbb306c25dc
TannerGarlick <gar18086@byui.edu>
2021-02-06 16:20:50 -0800

    lesson5 complete

---
+++ lesson5/js/day.js
@@ -0,0 +1,12 @@
+function announcement() {
+    var answer = document.getElementById("pancakes").classList;
+    var day = new Date();
+    var number = day.getDay();
+
+    if (number == 5) {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+document.getElementById("pancakes").innerHTML = number;
+}

fa7bd7561fe19d1eb1fbfb6f7d902d97a28afbef
TannerGarlick <gar18086@byui.edu>
2021-02-12 16:43:24 -0800

    week 6

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,3 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide");
-}
+const banner = document.getElementsByTagName("aside")[0];
+  (new Date().getDay9() == 5)? banner.style.display = "block" : banner.style.display ="none;"
+

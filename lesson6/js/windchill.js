eaa40fdfb9b8540970acb90be72055878d639383
TannerGarlick <gar18086@byui.edu>
2021-02-13 09:33:51 -0800

    compress images

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,9 +2,9 @@ const t = parseFloat(document.querySelector("#temp").textContent);
 const s = parseFloat(document.querySelector("#wind").textContent);
 var f;
 
-if (t <= 50 || s >= 3) {
-  f = 35.74 + 0.6215 * t - 35.75 * s ^ (0.16) + 0.4275 * t * s ^ (0.16);
-  document.write(f);
+if (t <= 50 && s >= 3) {
+  f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
+  document.getElementById("windchill").innerHTML = f.toPrecision(f);
 } else {
-  document.write("N/A");
+  document.getElementById("windchill").innerHTML = ("N/A");
 }

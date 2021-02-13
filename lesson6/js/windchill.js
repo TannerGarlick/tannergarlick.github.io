d1834af17bae44a60eb4a661912147fd1f9b7557
TannerGarlick <gar18086@byui.edu>
2021-02-13 08:59:44 -0800

    js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,7 +2,7 @@ const t = parseFloat(document.querySelector("#temp").textContent);
 const s = parseFloat(document.querySelector("#wind").textContent);
 var f;
 
-if (t >= 50 || s >= 3) {
+if (t <= 50 || s >= 3) {
   f = 35.74 + 0.6215 * t - 35.75 * s ^ (0.16) + 0.4275 * t * s ^ (0.16);
   document.write(f);
 } else {

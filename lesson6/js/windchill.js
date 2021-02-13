9f60cc4ff6118bc57376c37b51090783694a0e00
TannerGarlick <gar18086@byui.edu>
2021-02-13 09:36:25 -0800

    js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,7 +4,7 @@ var f;
 
 if (t <= 50 && s >= 3) {
   f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
-  document.getElementById("windchill").innerHTML = f.toPrecision(f);
+  document.getElementById("windchill").innerHTML = Math.round(f);
 } else {
   document.getElementById("windchill").innerHTML = ("N/A");
 }

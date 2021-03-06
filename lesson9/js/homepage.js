cf938fd23ab1e5b83ebf2138188fb9092694834d
TannerGarlick <gar18086@byui.edu>
2021-03-05 20:48:12 -0800

    home page fixes

--- lesson9/js/homepage.js
+++ lesson9/js/homepage.js
@@ -7,105 +7,36 @@ fetch(requestURL)
     //console.table(jsonObject);
     const towns = jsonObject['towns'];
 
-    const cards = document.querySelector('.cards')
+    const cards = document.querySelector('#towns')
 
     towns.forEach(town => {
+      if(town.name == 'Fish Haven' || town.name == 'Preston' || town.name == 'Soda Springs')
+      {
       let card = document.createElement("section");
+      let text = document.createElement('div');
       let h2 = document.createElement("h2");
       let h3 = document.createElement("h3");
       let p = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
       let pimg = document.createElement("img");
 
-      h2.innerHTML = `${town.name}`;
-      p.innerHTML = `${town.motto}`;
-      h3.innerHTML = `Year Founded: ${town.yearFounded} <br> <br> Population: ${town.currentPopulation} <br> <br> 
-        Annual Rain Fall: ${town.averageRainfall}`;
+      h2.innerHTML = town.name;
+      p3.innerHTML = town.motto;
+      h3.innerHTML = "Year Founded: " + town.yearFounded;
+        p.innerHTML = "Population: " + town.currentPopulation;
+        p2.innerHTML = "Annual Rain Fall: " + town.averageRainfall;
+        pimg.setAttribute('src', "images/" + town.photo);
+        pimg.setAttribute('alt', "Image of " + town.name);
 
-        /*card.append(h2);
-        card.append(h3);
-        card.append(p);
+        text.append(h2);
+        text.append(p3);
+        text.append(h3);
+        text.append(p);
+        text.append(p2);
+        card.append(text);
         card.append(pimg);
-        cards.append(card);*/
+        cards.append(card);
+      }
     });
-
-      const preston = document.querySelector('.preston');
-      
-      const listfilter = towns.filter(x => x.name == "Preston");
-
-      listfilter.forEach(prestontown => {
-        let card = document.createElement("section");
-        let h2 = document.createElement("h2");
-        let h3 = document.createElement("h3");
-        let p = document.createElement("p");
-        let pimg = document.createElement("img");
-  
-        h2.innerHTML = `${prestontown.name}`;
-        p.innerHTML = `${prestontown.motto}`;
-        h3.innerHTML = `Year Founded: ${prestontown.yearFounded} <br> <br> Population: ${prestontown.currentPopulation} <br> <br> 
-          Annual Rain Fall: ${prestontown.averageRainfall}`;
-
-          pimg.setAttribute('src', `images/${prestontown.photo}`);
-          pimg.setAttribute('alt', `Image of ${prestontown.name}`);
-          pimg.style.width = '450px';
-  
-          card.append(pimg);
-          card.append(h2);
-          card.append(p);
-          card.append(h3);
-          preston.append(card);
-      });
-
-      const fishhaven = document.querySelector('.fishhaven');
-      
-      const listfilter2 = towns.filter(y => y.name == "Fish Haven");
-
-      listfilter2.forEach(fishhaventown => {
-        let card = document.createElement("section");
-        let h2 = document.createElement("h2");
-        let h3 = document.createElement("h3");
-        let p = document.createElement("p");
-        let pimg = document.createElement("img");
-  
-        h2.innerHTML = `${fishhaventown.name}`;
-        p.innerHTML = `${fishhaventown.motto}`;
-        h3.innerHTML = `Year Founded: ${fishhaventown.yearFounded} <br> <br> Population: ${fishhaventown.currentPopulation} <br> <br> 
-          Annual Rain Fall: ${fishhaventown.averageRainfall}`;
-
-          pimg.setAttribute('src', `images/${fishhaventown.photo}`);
-          pimg.setAttribute('alt', `Image of ${fishhaventown.name}`);
-          pimg.style.width = '450px';
-  
-          card.append(pimg);
-          card.append(h2);
-          card.append(p);
-          card.append(h3);
-          fishhaven.append(card);
-      });
-
-      const sodasprings = document.querySelector('.sodasprings');
-      
-      const listfilter3 = towns.filter(z => z.name == "Soda Springs");
-
-      listfilter3.forEach(sodaspringstown => {
-        let card = document.createElement("section");
-        let h2 = document.createElement("h2");
-        let h3 = document.createElement("h3");
-        let p = document.createElement("p");
-        let pimg = document.createElement("img");
-  
-        h2.innerHTML = `${sodaspringstown.name}`;
-        p.innerHTML = `${sodaspringstown.motto}`;
-        h3.innerHTML = `Year Founded: ${sodaspringstown.yearFounded} <br> <br> Population: ${sodaspringstown.currentPopulation} <br> <br> 
-          Annual Rain Fall: ${sodaspringstown.averageRainfall}`;
-
-          pimg.setAttribute('src', `images/${sodaspringstown.photo}`);
-          pimg.setAttribute('alt', `Image of ${sodaspringstown.name}`);
-          pimg.style.width = '450px';
-  
-          card.append(pimg);
-          card.append(h2);
-          card.append(p);
-          card.append(h3);
-          sodasprings.append(card);
-      });
-    });
+});
